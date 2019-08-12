83a25eccec792a8249326d655f3748b2b77a1987
sammui4 <490454041@qq.com>
2019-08-12 16:40:30 +0800

    配置文件修改

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: w
  * @Date: 2019-08-05 16:11:20
  * @LastEditors: w
- * @LastEditTime: 2019-08-10 19:00:41
+ * @LastEditTime: 2019-08-12 16:39:11
  */
 const path = require('path');
 
@@ -50,12 +50,18 @@ module.exports = {
           url: "http://localhost:888/client"
         },
         nsis: {
-          oneClick: false,
-          perMachine: true,
-          allowElevation: true,
-          allowToChangeInstallationDirectory: true,
-          createDesktopShortcut: true,
-          runAfterFinish: true,
+          oneClick: false, // 是否一键安装
+          perMachine: true,         // 是否为辅助安装程序显示安装模式安装程序页面（每台计算机或每用户选择）。或者是否始终按所有用户（每台机器）安装
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          // installerIcon: "./build/icons/aaa.ico",// 安装图标
+          // uninstallerIcon: "./build/icons/bbb.ico",//卸载图标
+          // installerHeaderIcon: "./build/icons/aaa.ico", // 安装时头部图标
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true,// 创建开始菜单图标
+          // shortcutName: "xxxx", // 图标名称
+          // include: "build/script/installer.nsh", // 包含的自定义nsis脚本
+          runAfterFinish: true,     // 完成后是否运行已安装的应用程序
         },
       }
     }
