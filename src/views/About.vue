65b3dc71ca4be0b8c1177a5f623ab34f9d53583b
sammui4 <490454041@qq.com>
2019-08-13 17:11:46 +0800

    try

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Author: w
+ * @Date: 2019-08-05 16:11:20
+ * @LastEditors: w
+ * @LastEditTime: 2019-08-13 17:01:44
+ -->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>我就新增一句话玩玩</p>
   </div>
 </template>
